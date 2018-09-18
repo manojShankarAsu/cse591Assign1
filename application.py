6eeb1f630c62936f37433c65c56c5a3f13cb5fb2
manojShankarAsu <manandav@asu.edu>
2018-09-18 16:54:49 -0700

    profile page created

--- application.py
+++ application.py
@@ -131,6 +131,16 @@ def register():
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
 
+@application.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+        {'author': user, 'body': 'Test post #1'},
+        {'author': user, 'body': 'Test post #2'}
+    ]
+    return render_template('user.html', user=user, posts=posts)
+
 # run the app.
 if __name__ == "__main__":
     # Setting debug to True enables debug output. This line should be
