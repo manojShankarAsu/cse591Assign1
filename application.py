95b9770e41311181d159fcf3a50cb58f55e85a41
manojShankarAsu <manandav@asu.edu>
2018-09-18 14:38:16 -0700

    Login with DBs added

--- application.py
+++ application.py
@@ -1,52 +1,85 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
+from config import Config
+from forms import LoginForm
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
+from flask_login import current_user, login_user, logout_user
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 import sqlite3
+from datetime import datetime
 
-sqlite_file = 'my_first_db.sqlite' 
-table_name1 = 'table1'
-col1 = 'col1'
-ftype = 'TEXT'
 
-conn = sqlite3.connect(sqlite_file)
-c = conn.cursor()
-
-# print a nice greeting.
-def say_hello(username = "World"):
-    return '<p>Hello %s!</p>\n' % username
-
-# some bits of text for the page.
-header_text = '''
-    <html>\n<head> <title>EB Flask Test</title> </head>\n<body>'''
-instructions = '''
-    <p><em>Hint</em>: This is a MADE CHANGES RESTful web service! Append a username
-    to the URL (for example: <code>/Thelonious</code>) to say hello to
-    someone specific.</p>\n'''
-home_link = '<p><a href="/">Back</a></p>\n'
-footer_text = '</body>\n</html>'
-
-user = {'username': 'Miguel'}
-
+user = {'username': 'Manoj'}
 # EB looks for an 'application' callable by default.
 application = Flask(__name__)
+application.config.from_object(Config)
+db = SQLAlchemy(application)
+migrate = Migrate(application, db)
+login = LoginManager(application)
+
+def create_tables():
+    db.create_all()
+
+class User(UserMixin,db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Post {}>'.format(self.body)
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
 
 # add a rule for the index page.
 application.add_url_rule('/', 'index', (lambda: render_template('index.html', title='Home', user=user)))
 
-# add a rule when the page is accessed with a name appended to the site
-# URL.
-application.add_url_rule('/<username>', 'hello', (lambda username:
-    header_text + say_hello(username) + home_link + footer_text))
+@application.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
+
+@application.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
 
 # run the app.
 if __name__ == "__main__":
     # Setting debug to True enables debug output. This line should be
     # removed before deploying a production app.
     application.debug = True
-    c.execute('CREATE TABLE {tn} ({nf} {ft})'.format(tn=table_name1, nf=col1, ft=ftype))
-    conn.commit()
-    try:
-    c.execute("INSERT INTO {tn} ({cn}) VALUES ('test')".format(tn=table_name, cn=col1))
-    except sqlite3.IntegrityError:
-    print('ERROR: ID already exists in PRIMARY KEY column {}'.format(id_column))
-
+    create_tables()
     application.run()
