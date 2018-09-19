756b861d75a04b3f03bac2383b9fcf6a1af910f1
manojShankarAsu <manandav@asu.edu>
2018-09-18 18:15:40 -0700

    Last Seen add in profile page

--- application.py
+++ application.py
@@ -31,6 +31,8 @@ class User(UserMixin,db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -92,6 +94,12 @@ def create_tables():
 def load_user(id):
     return User.query.get(int(id))
 
+
+@application.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
 
 # add a rule for the index page.
 
